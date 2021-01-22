056ee7040f9f914f3d50c70c87ec49f73119dc67
darryncampbell <darryncampbell@hotmail.com>
2021-01-22 17:22:08 +0000

    Initial checkin of app

---
+++ DataWedgeExpo/android/app/src/main/java/com/datawedgereactnative/demo/MainApplication.java
@@ -0,0 +1,65 @@
+package com.datawedgereactnative.demo;
+
+// com.myapp should be your package name
+import com.datawedgereactnative.demo.generated.BasePackageList;
+ 
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.List;
+import java.util.Arrays;
+ 
+import org.unimodules.adapters.react.ModuleRegistryAdapter;
+import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+import org.unimodules.core.interfaces.SingletonModule;
+
+public class MainApplication extends Application implements ReactApplication {
+    private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      //packages.add(new RNDataWedgeIntentsPackage());
+      //packages.add(new VectorIconsPackage());
+       
+          // Add unimodules
+          List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
+            new ModuleRegistryAdapter(mModuleRegistryProvider)
+          );
+          packages.addAll(unimodules);
+      return packages;
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # DataWedge-Expo-Sample
 Sample application written with the Expo framework to show barcode scanning on Zebra mobile computers 
+
+Notes:
+- https://github.com/darryncampbell/react-native-datawedge-intents/issues/14
+- https://docs.expo.io/bare/exploring-bare-workflow/
+- expo init DataWedgeExpo
+- I selected 'bare and minimal, just the essentials to get started'
+- cd DataWedgeExpo
+- yarn add react-native-datawedge-intents
+- yarn android (to run)
+
+NEW PLAN
+- Add some expo functionality to the existing DataWedge React Native sample
+- Cloned existing DataWedgeReactNative sample
+- yarn add react-native-unimodules
+- yarn install replaces npm install
+- Had to add to my package.json an android script, "react-native run-android"
+- Had to add my debug.keystore to DataWedgeExpo\android\app
+- To demo Expo SDK will use https://docs.expo.io/versions/v40.0.0/sdk/device/
+- expo install expo-device
+- Device manufacturer is returning NULL... I needed to follow https://docs.expo.io/bare/installing-unimodules/
+
+todo:
+Create blog - main point is you need to use the bare workflow because the Intent API offered by Expo does not have enough functionality to control the DataWedge API.  ALSO, KNOWN ISSUES 
